f7b24c08f63e006b141118062e449e4d52b6f322
khoerul-mutaqin <khoerul.mutaqin@falinwa.com>
2026-01-20 16:59:03 +0700

    [init] purchase_order_form

---
+++ purchase_order_form/__manifest__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Invoice Penjualan JITU Form",
+    "version": "18.0.0.0.0",
+    'license': 'OPL-1',
+    'summary': 'Invoice Penjualan JITU Form',
+    "category": 'Invoicing',
+    "depends": ['account'],
+}
